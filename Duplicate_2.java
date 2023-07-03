45d2f760f0e8452aa096f7d2ef09ccb6bb3cc4af
Ganesh <ganesh02003@gmai.com>
2023-07-03 21:05:14 +0530

    Checking nearby Duplicate

---
+++ Duplicate_2.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Duplicate_2 {
+    //Method
+     public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                if(Math.abs(map.get(nums[i])-i)<=k){
+                    return true;
+                }
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+    
+    //Main Method
+    public static void main(String[] args) {
+        int nums[] = {1,0,1,1};
+        System.out.println(containsNearbyDuplicate(nums,1));
+    }
+}
